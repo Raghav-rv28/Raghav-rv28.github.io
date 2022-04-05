d0b12a508ad1b68952365c48439cd146e05cf802
Raghav-rv28 <Raghavrudhra28@gmail.com>
2022-04-04 22:05:30 -0400

    ADDING PROJECTS

--- src/Components/Projects/data.jsx
+++ src/Components/Projects/data.jsx
@@ -1,48 +1,86 @@
-import Proj from '../../assets/images/Projects/tenzies-game.jpg'
-import Proj2 from '../../assets/images/Projects/notes-app.jpg'
-import Proj3 from '../../assets/images/Projects/computer_won.jpg'
-
+import tenzies from '../../assets/images/Projects/tenzies-game.jpg'
+import notesapp from '../../assets/images/Projects/notes-app.jpg'
+import comp from '../../assets/images/Projects/computer_won.jpg'
+import muskaan from '../../assets/images/Projects/muskaanrudhra.jpg'
+import portfolio from '../../assets/images/Projects/portfolio.jpg'
+import dubai from '../../assets/images/Projects/dubaijewellers.jpg'
 
 export const AllprojectsData = [
     {
-        imgsrc: Proj,
+        imgsrc: tenzies,
         title: "Tenzies Game",
         codeLink: "https://github.com/Raghav-rv28/tenzies-game",
         id: "Web"
 
     },
     {
-        imgsrc: Proj2,
+        imgsrc: notesapp,
         title: "Notes App",
         codeLink: "https://github.com/Raghav-rv28/notes-app",
         id: "Web"
     },
     {
-        imgsrc: Proj3,
+        imgsrc: comp,
         title: "Tic-Tac-Toe",
         codeLink: "https://github.com/Raghav-rv28/tenzies-game",
         id: "Software"
     },
+    {
+        imgsrc: muskaan,
+        title: "Wordpress Website",
+        codeLink: "https://Muskaanrudhra.com",
+        id: "Web"
+    },
+    {
+        imgsrc: portfolio,
+        title: "portfolio website",
+        codeLink: "https://Raghav-rv28.github.io/portfolio",
+        id: "Web"
+    },
+    {
+        imgsrc: dubai,
+        title: "E-comm Wordpress Website",
+        codeLink: "https://dubaijewellers.ca",
+        id: "Web"
+    }
 ]
 export const WebProjectsData = [
     {
-        imgsrc: Proj,
+        imgsrc: tenzies,
         title: "Tenzies Game",
         codeLink: "https://github.com/Raghav-rv28/tenzies-game",
         id: "Web"
 
     },
     {
-        imgsrc: Proj2,
+        imgsrc: notesapp,
         title: "Notes App",
         codeLink: "https://github.com/Raghav-rv28/notes-app",
+        id: "Web"
+    },
+    {
+        imgsrc: muskaan,
+        title: "Wordpress Website",
+        codeLink: "https://Muskaanrudhra.com",
+        id: "Web"
+    },
+    {
+        imgsrc: portfolio,
+        title: "portfolio website",
+        codeLink: "https://Raghav-rv28.github.io/portfolio",
+        id: "Web"
+    },
+    {
+        imgsrc: dubai,
+        title: "E-comm Wordpress Website",
+        codeLink: "https://dubaijewellers.ca",
         id: "Web"
     }
 ]
 
 export const SoftwareProjectsData = [
     {
-        imgsrc: Proj3,
+        imgsrc: comp,
         title: "Tic-Tac-Toe",
         codeLink: "https://github.com/Raghav-rv28/tenzies-game",
         id: "Software"
